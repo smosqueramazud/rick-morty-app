6680ecc949769b36b887f08326a1b568dd5135ec
Sebastian Mosquera <sebastian.mosquera@supergiros.com.co>
2022-11-15 18:37:45 -0500

    se crea el inicio y header como componentes @Author Sebastian Mosquera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { HeaderAppComponent } from './components/header-app/header-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderAppComponent
   ],
   imports: [
     BrowserModule,
